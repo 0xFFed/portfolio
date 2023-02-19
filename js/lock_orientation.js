3e279f044baff2dca77888437446dfbb4639ce6c
0xFFed <thenedbear@hotmail.it>
2023-02-20 00:45:30 +0100

    Added script to lock orientation to portrait Added code to prevent the URL bar from disappearing on mobile devices

---
+++ js/lock_orientation.js
@@ -0,0 +1,3 @@
+/* Locks the orientation mode to portrait for mobile */
+
+screen.orientation.lock("portrait");
--- js/page_height.js
+++
@@ -1,7 +0,0 @@
-/* Handles mobile visualization problems */
-
-const setPageHeight = () => {
-    document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`)
-}
-window.addEventListener('resize', setPageHeight)
-setPageHeight()
