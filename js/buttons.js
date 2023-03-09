345f1c41cb7cba158ea3dbd17f3ef40bdd1a996f
0xFFed <thenedbear@hotmail.it>
2023-03-09 01:06:23 +0100

    Programmed hero-section buttons behavior

--- js/buttons.js
+++ js/buttons.js
@@ -27,3 +27,23 @@ const systemSection = document.querySelector('#section-system');
 if(systemButton) systemButton.addEventListener('click', () => {
     systemSection.scrollIntoView();
 });
+
+
+// mail button behavior
+const MAIL = 'fred.ef.dev@gmail.com';
+const mailButton = document.querySelector('#hero-button-4');
+const copyText = document.querySelector('#button-4-pop-up');
+let isMailCopyOn = false;
+if(mailButton) mailButton.addEventListener('click', () => {
+    navigator.clipboard.writeText(MAIL);
+
+    // fading in-and-out the "Copied" text
+    if(!isMailCopyOn) {
+        copyText.style.opacity="1";
+        isMailCopyOn = true;
+        setTimeout(() => {
+            copyText.style.opacity="0";
+            isMailCopyOn = false;
+        }, 1000);
+    }
+})
